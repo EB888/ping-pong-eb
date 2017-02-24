cff5cddfc882f96196b3f245a9f749f1ecb7ae4b
Evan Bishop <evan.bishop888@gmail.com>
2017-02-24 13:37:21 -0800

    pass sixth spec of emptying output of previous results when resubmitting form

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,28 @@
 $(document).ready(function(){
-  var userInput = parseInt($("#pingpong").val());
   var pingPong = function(input){
     var i = 1;
     while  (i <= input){
       if (i % 15 === 0){
         // console.log("Divisible by 15")
-        $("ul#output").append("<li>" + "Ping-Pong" + "</li>");
+        $("ul#outputul").append("<li>" + "---> $" + "Ping-Pong" + "</li>");
         i++
       }else if(i % 5 === 0){
-        $("ul#output").append("<li>" + "Pong" + "</li>");
+        $("ul#outputul").append("<li>" + "---> $" + "Pong" + "</li>");
         i++
       }else if(i % 3 === 0){
-        $("ul#output").append("<li>" + "Ping" + "</li>");
+        $("ul#outputul").append("<li>" + "---> $" + "Ping" + "</li>");
         i++
       }else{
         // console.log(i)
-        $("ul#output").append("<li>" + i + "</li>");
+        $("ul#outputul").append("<li>" + "---> $" + i + "</li>");
         i++
       }
     }
   }
   $("#pingpongForm").submit(function(event){
     event.preventDefault();
+    $("ul#outputul").empty();
+    var userInput = parseInt($("#pingpong").val());
     pingPong(userInput);
   });
 });
