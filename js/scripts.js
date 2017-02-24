da6ad02dde8d43a486db20ccf78abf5baf8a57a4
Evan Bishop <evan.bishop888@gmail.com>
2017-02-24 14:34:20 -0800

    add branching statements in case user input is 666

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,37 @@
+// BACKEND LOGIC
 $(document).ready(function(){
-  var pingPong = function(input){
-    var i = 1;
-    while  (i <= input){
-      if (i % 15 === 0){
-        // console.log("Divisible by 15")
-        $("ul#outputul").append("<li>" + "---> $" + "Ping-Pong" + "</li>");
-        i++
-      }else if(i % 5 === 0){
-        $("ul#outputul").append("<li>" + "---> $" + "Pong" + "</li>");
-        i++
-      }else if(i % 3 === 0){
-        $("ul#outputul").append("<li>" + "---> $" + "Ping" + "</li>");
-        i++
-      }else{
-        // console.log(i)
-        $("ul#outputul").append("<li>" + "---> $" + i + "</li>");
-        i++
-      }
+function pingPong(input){
+  var i = 1;
+  while  (i <= input){
+    if (i % 15 === 0){
+      // console.log("Divisible by 15")
+      $("ul#outputul").append("<li>" + "---> $ " + "Ping-Pong" + "</li>");
+      i++
+    }else if(i % 5 === 0){
+      $("ul#outputul").append("<li>" + "---> $ " + "Pong" + "</li>");
+      i++
+    }else if(i % 3 === 0){
+      $("ul#outputul").append("<li>" + "---> $ " + "Ping" + "</li>");
+      i++
+    }else{
+      // console.log(i)
+      $("ul#outputul").append("<li>" + "---> $ " + i + "</li>");
+      i++
     }
   }
+}
+// FRONT END LOGIC
   $("#pingpongForm").submit(function(event){
     event.preventDefault();
-    $("ul#outputul").empty();
+    $("ul#outputul").empty(); //reset ul element before re-running ping pong function
+    $(".outputdiv").hide();
+    $("#pinginThePong").hide();
     var userInput = parseInt($("#pingpong").val());
-    pingPong(userInput);
+    if (userInput === 666){
+      $(".outputdiv").slideDown(1000);
+    }else{
+      pingPong(userInput);
+      $("#pinginThePong").slideDown(500);
+    }
   });
 });
