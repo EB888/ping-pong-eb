ef614769090db23df773b1b4446c7ca9b400538c
Evan Bishop <evan.bishop888@gmail.com>
2017-03-01 18:06:31 -0800

    separate backend and front end javascript into functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,59 @@
 // BACKEND LOGIC
-$(document).ready(function(){
+function pingPong(input){
   var ping = "Ping";
   var pong = "Pong";
-  var pingPong = "Ping-Pong";
-  var pingPongArray =[];
+  var pingPongVar = "Ping-Pong";
+  var pingPongArray =[]
+  var i = 1;
 
-  function pingPong(input){
-    var i = 1;
-    while  (i <= input){
-      if (i % 15 === 0){
-        pingPongArray.push(pingPong);
-        // $("ul#outputul").append("<li>" + pingPong + "</li>");
-        i++
-      }else if(i % 5 === 0){
-        pingPongArray.push(pong);
-        // $("ul#outputul").append("<li>" + Pong + "</li>");
-        i++
-      }else if(i % 3 === 0){
-        pingPongArray.push(ping);
-        // $("ul#outputul").append("<li>" + Ping + "</li>");
-        i++
-      }else{
-        pingPongArray.push(i);
-        // $("ul#outputul").append("<li>" + i + "</li>");
-        i++
-      }
+  while  (i <= input){
+    if (i % 15 === 0){
+      pingPongArray.push(pingPongVar);
+      // $("ul#outputul").append("<li>" + pingPong + "</li>");
+      i++
+    }else if(i % 5 === 0){
+      pingPongArray.push(pong);
+      // $("ul#outputul").append("<li>" + Pong + "</li>");
+      i++
+    }else if(i % 3 === 0){
+      pingPongArray.push(ping);
+      // $("ul#outputul").append("<li>" + Ping + "</li>");
+      i++
+    }else{
+      pingPongArray.push(i);
+      // $("ul#outputul").append("<li>" + i + "</li>");
+      i++
     }
   }
+  return pingPongArray;
+}
+
+
+
 // FRONT END LOGIC
+$(document).ready(function(){
   $("#pingpongForm").submit(function(event){
     event.preventDefault();
-    $("ul#outputul").empty(); //reset ul element before re-running ping pong function
-    $(".outputdiv").hide(); // Hide 666 Picture
-    $("#pinginThePong").hide(); //Hide Ping Pong Picture
+    //reset ul element before re-running ping pong function
+    $("ul#outputul").empty();
+    //Hide 666 Picture
+    $(".outputdiv").hide();
+    //Hide Ping Pong Picture
+    $("#pinginThePong").hide();
+
     var userInput = parseInt($("#pingpong").val());
-    // if (userInput === 666){
-    //   $(".outputdiv").slideDown(1000);
-    // }else{
-      pingPong(userInput);
-    //   $("#pinginThePong").slideDown(500);
-    pingPongArray.forEach(function(number) {
-      $("ul#outputul").append("<li>" + number + "</li>");
-    });
 
+    if (userInput === 666){
+      $(".outputdiv").slideDown(1000);
+    }else{
+      $("#pinginThePong").slideDown(500);
 
-
+      var pingPongResult = pingPong(userInput);
+      pingPongResult.forEach(function(number) {
+        $("ul#outputul").append("<li>" + number + "</li>");
+      });
     }
+
   });
+
 });
