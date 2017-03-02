e58c59b8243606bbc3cf95a2f6c87e2851d406df
Evan Bishop <evan.bishop888@gmail.com>
2017-03-01 16:57:53 -0800

    attempt to rework javascript into UI and Backend  after failing to meet expectations

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,50 @@
 // BACKEND LOGIC
 $(document).ready(function(){
-function pingPong(input){
-  var i = 1;
-  while  (i <= input){
-    if (i % 15 === 0){
-      // console.log("Divisible by 15")
-      $("ul#outputul").append("<li>" + "---> $ " + "Ping-Pong" + "</li>");
-      i++
-    }else if(i % 5 === 0){
-      $("ul#outputul").append("<li>" + "---> $ " + "Pong" + "</li>");
-      i++
-    }else if(i % 3 === 0){
-      $("ul#outputul").append("<li>" + "---> $ " + "Ping" + "</li>");
-      i++
-    }else{
-      // console.log(i)
-      $("ul#outputul").append("<li>" + "---> $ " + i + "</li>");
-      i++
+  var ping = "Ping";
+  var pong = "Pong";
+  var pingPong = "Ping-Pong";
+  var pingPongArray =[];
+
+  function pingPong(input){
+    var i = 1;
+    while  (i <= input){
+      if (i % 15 === 0){
+        pingPongArray.push(pingPong);
+        // $("ul#outputul").append("<li>" + pingPong + "</li>");
+        i++
+      }else if(i % 5 === 0){
+        pingPongArray.push(pong);
+        // $("ul#outputul").append("<li>" + Pong + "</li>");
+        i++
+      }else if(i % 3 === 0){
+        pingPongArray.push(ping);
+        // $("ul#outputul").append("<li>" + Ping + "</li>");
+        i++
+      }else{
+        pingPongArray.push(i);
+        // $("ul#outputul").append("<li>" + i + "</li>");
+        i++
+      }
     }
   }
-}
 // FRONT END LOGIC
   $("#pingpongForm").submit(function(event){
     event.preventDefault();
     $("ul#outputul").empty(); //reset ul element before re-running ping pong function
-    $(".outputdiv").hide();
-    $("#pinginThePong").hide();
+    $(".outputdiv").hide(); // Hide 666 Picture
+    $("#pinginThePong").hide(); //Hide Ping Pong Picture
     var userInput = parseInt($("#pingpong").val());
-    if (userInput === 666){
-      $(".outputdiv").slideDown(1000);
-    }else{
+    // if (userInput === 666){
+    //   $(".outputdiv").slideDown(1000);
+    // }else{
       pingPong(userInput);
-      $("#pinginThePong").slideDown(500);
+    //   $("#pinginThePong").slideDown(500);
+    pingPongArray.forEach(function(number) {
+      $("ul#outputul").append("<li>" + number + "</li>");
+    });
+
+
+
     }
   });
 });
